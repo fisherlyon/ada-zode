730cb194130dad2f3df01a4c7b0d9f3a0b9d117b
fisherlyon <fwlyon@calpoly.edu>
2024-06-06 23:37:54 -0700

    maybe done

--- src/zode5.adb
+++ src/zode5.adb
@@ -1,4 +1,3 @@
-with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Assertions; use Ada.Assertions;
 
@@ -43,8 +42,8 @@ procedure Zode5 is
          when StrV =>
             Str : Unbounded_String;
          when CloV =>
-            Param : Unbounded_String;
-            
+            Param1 : Unbounded_String;
+            Param2 : Unbounded_String;
             Bod : ExprC_Acc;
             Env : EnvNode_Acc;
          when PrimV =>
@@ -65,11 +64,12 @@ procedure Zode5 is
             Th : ExprC_Acc;
             El : ExprC_Acc;
          when LamC =>
-            Param : Unbounded_String;
+            Param1 : Unbounded_String;
+            Param2 : Unbounded_String;
             Bod : ExprC_Acc;
          when AppC =>
             Fun : ExprC_Acc;
-            Arg : ExprC_Acc;
+            Arg1 : ExprC_Acc;
             Arg2 : ExprC_Acc;
       end case;
    end record;
@@ -84,17 +84,14 @@ procedure Zode5 is
    function Extend_Env
       (Env : EnvNode_Acc; Param : Unbounded_String; Val : Value_Acc)
          return EnvNode_Acc is
+      Cur : EnvNode_Acc := Env;
    begin
-      declare
-         Cur : EnvNode_Acc := Env;
-      begin
-         while Cur.Next /= null loop
-            Cur := Cur.Next;
-         end loop;
-         Cur.Next := new EnvNode'(Content =>
-            Binding'(Name => Param, Val => Val),
-         Next => null);
-      end;
+      while Cur.Next /= null loop
+         Cur := Cur.Next;
+      end loop;
+      Cur.Next := new EnvNode'(Content =>
+         Binding'(Name => Param, Val => Val),
+      Next => null);
       return Env;
    end Extend_Env;
 
@@ -111,49 +108,42 @@ procedure Zode5 is
 
    --  Creates the Top Level Environment
    function Create_TLE return EnvNode_Acc is
-   begin
-      declare
-         TLE : EnvNode_Acc :=
+      TLE : EnvNode_Acc :=
             new EnvNode'(Content =>
                Binding'(Name => Strify ("+"),
                Val =>
                   new Value'(Kind => PrimV, Op => Strify ("+"))),
             Next => null);
-      begin
-         for J in Primops_Array'Range loop
-            TLE := Extend_Env (TLE,
-               Primops_Array (J), new Value'(Kind => PrimV,
-                  Op => Primops_Array (J)));
-         end loop;
-         TLE := Extend_Env (TLE, Strify ("true"),
-             new Value'(Kind => BoolV, Bool => True));
-         TLE := Extend_Env (TLE, Strify ("false"),
-             new Value'(Kind => BoolV, Bool => False));
-         return TLE;
-      end;
+   begin
+      for J in Primops_Array'Range loop
+         TLE := Extend_Env (TLE,
+            Primops_Array (J), new Value'(Kind => PrimV,
+               Op => Primops_Array (J)));
+      end loop;
+      TLE := Extend_Env (TLE, Strify ("true"),
+            new Value'(Kind => BoolV, Bool => True));
+      TLE := Extend_Env (TLE, Strify ("false"),
+            new Value'(Kind => BoolV, Bool => False));
+      return TLE;
    end Create_TLE;
 
    --  Looks up an identifier in a environment and rerturns its value
-   function Lookup
-      (Id : Unbounded_String; Env : EnvNode_Acc) return Value_Acc is
+   function Lookup (Id : Unbounded_String; Env : EnvNode_Acc)
+      return Value_Acc is
+      Cur : EnvNode_Acc := Env;
    begin
-
-      declare
-         Cur : EnvNode_Acc := Env;
-      begin
          while Cur /= null loop
             if Id = Cur.Content.Name then
                return Cur.Content.Val;
             end if;
             Cur := Cur.Next;
          end loop;
-         Put_Line ("variable not found: " & To_String (Id));
+         --  Put_Line ("variable not found: " & To_String (Id));
          raise Constraint_Error;
-      end;
    end Lookup;
 
-   -- Evaluates PrimV
-   function Eval (Op: Unbounded_String ; l : Value_Acc ; r : Value_Acc) 
+   --  Evaluates PrimV
+   function Eval (Op : Unbounded_String; l : Value_Acc; r : Value_Acc)
       return Value_Acc is
    begin
       if Op = "+" then
@@ -164,8 +154,14 @@ procedure Zode5 is
          return new Value'(Kind => NumV, Val => (l.Val * r.Val));
       elsif Op = "/" then
          return new Value'(Kind => NumV, Val => (l.Val / r.Val));
+      elsif Op = "<=" then
+         return new Value'(Kind => BoolV, Bool => (l.Val <= r.Val));
+      elsif Op = "equal?" then
+         return new Value'(Kind => BoolV, Bool => (l.Val = r.Val));
+      elsif Op = "error" then
+         raise Program_Error;
       else
-         raise Program_Error; 
+         raise Program_Error;
       end if;
    end Eval;
 
@@ -173,7 +169,6 @@ procedure Zode5 is
    function Interp (Expr : ExprC_Acc; Env : EnvNode_Acc)
       return Value_Acc is
    begin
-
       case Expr.Kind is
          when NumC =>
             return new Value'(Kind => NumV, Val => Expr.Val);
@@ -183,7 +178,7 @@ procedure Zode5 is
             return new Value'(Kind => StrV, Str => Expr.Str);
          when IfC =>
             declare
-               Interped_Test : Value_Acc := Interp (Expr.Te, Env);
+               Interped_Test : constant Value_Acc := Interp (Expr.Te, Env);
             begin
                if Interped_Test.Bool = True then
                   return Interp (Expr.Th, Env);
@@ -193,40 +188,36 @@ procedure Zode5 is
             end;
          when LamC =>
             return new Value'(Kind => CloV,
-               Param => Expr.Param,
+               Param1 => Expr.Param1,
+               Param2 => Expr.Param2,
                Bod => Expr.Bod,
                Env => Env);
          when AppC =>
-
             declare
-
-               Fun_Val : Value_Acc := Interp (Expr.Fun, Env);
-               Arg_Val1 : Value_Acc := Interp (Expr.Arg, Env);
-               Arg_Val2 : Value_Acc := Interp (Expr.Arg2, Env);
-
-               
-               begin
-                  case Fun_Val.kind is
-                     when CloV =>
-                        declare
-                           Env2 : EnvNode_Acc :=
-                           Extend_Env (Env, Fun_Val.Param, Arg_Val1);
-
-                        begin
-                           return Interp (Fun_Val.Bod, Env2);
-                        end;
-                     when PrimV =>
-                        return Eval (Fun_Val.Op, Arg_Val1, Arg_Val2);
-                     when NumV =>
-                        raise Program_Error; 
-                     when BoolV =>
-                        raise Program_Error; 
-                     when StrV =>
-                        raise Program_Error; 
-                     
-                  end case;
-
-               end;
+               Fun_Val : constant Value_Acc := Interp (Expr.Fun, Env);
+               Arg_Val1 : constant Value_Acc := Interp (Expr.Arg1, Env);
+               Arg_Val2 : constant Value_Acc := Interp (Expr.Arg2, Env);
+            begin
+               case Fun_Val.Kind is
+                  when CloV =>
+                     declare
+                        Env2 : EnvNode_Acc :=
+                           Extend_Env (Env, Fun_Val.Param1, Arg_Val1);
+                     begin
+                        Env2 :=
+                           Extend_Env (Env2, Fun_Val.Param2, Arg_Val2);
+                        return Interp (Fun_Val.Bod, Env2);
+                     end;
+                  when PrimV =>
+                     return Eval (Fun_Val.Op, Arg_Val1, Arg_Val2);
+                  when NumV =>
+                     raise Program_Error;
+                  when BoolV =>
+                     raise Program_Error;
+                  when StrV =>
+                     raise Program_Error;
+               end case;
+            end;
       end case;
    end Interp;
 
@@ -264,7 +255,7 @@ procedure Zode5 is
    -- Example Instances --
    -----------------------
 
-   Envr_Ex : EnvNode_Acc :=
+   Envr_Ex : constant EnvNode_Acc :=
       new EnvNode'(Content =>
          Binding'(Name => Strify ("nice"),
             Val => new Value'(Kind => NumV, Val => 6.9)),
@@ -273,13 +264,24 @@ procedure Zode5 is
                Val => new Value'(Kind => StrV, Str => Strify ("cool"))),
             Next => null));
 
-   Envr_Ex2 : EnvNode_Acc :=
+   Envr_Ex2 : constant EnvNode_Acc :=
       new EnvNode'(Content =>
          Binding'(Name => Strify ("x"),
             Val => new Value'(Kind => NumV, Val => 1.0)),
          Next => null);
 
+   LamC_Ex1 : constant ExprC_Acc :=
+      new ExprC'(Kind => LamC,
+         Param1 => Strify ("x"),
+         Param2 => Strify ("y"),
+         Bod =>
+            new ExprC'(Kind => AppC,
+               Fun => new ExprC'(Kind => IdC, Id => Strify ("<=")),
+               Arg1 => new ExprC'(Kind => IdC, Id => Strify ("x")),
+               Arg2 => new ExprC'(Kind => IdC, Id => Strify ("y"))));
+
 begin
+
    ----------------
    -- Test Cases --
    ----------------
@@ -311,46 +313,56 @@ begin
 
       --  Serialize tests
       Assert (Serialize
+         (new Value'(Kind => NumV, Val => 4.0)) = Float'Image (4.0));
+      Assert (Serialize
          (new Value'(Kind => BoolV, Bool => True)) = "true");
       Assert (Serialize
          (new Value'(Kind => BoolV, Bool => False)) = "false");
       Assert (Serialize
          (new Value'(Kind => StrV, Str => Strify ("Hello"))) = "Hello");
       Assert (Serialize
-         (new Value'(Kind => CloV, Param => Strify ("x"),
+         (new Value'(Kind => CloV,
+            Param1 => Strify ("x"),
+            Param2 => Strify ("y"),
             Bod => new ExprC'(Kind => NumC, Val => 4.0),
             Env => Envr_Ex)) = "#<procedure>");
       Assert (Serialize
          (new Value'(Kind => PrimV, Op => Strify ("+"))) = "#<primop>");
 
-      -- Eval tests
-      
+      --  Eval  tests
       Assert (Eval
-         (Strify ("+"), new Value'(Kind => NumV, Val => 2.0), 
+         (Strify ("+"), new Value'(Kind => NumV, Val => 2.0),
          new Value'(Kind => NumV, Val => 2.0)).Val = 4.0);
       Assert (Eval
-         (Strify ("-"), new Value'(Kind => NumV, Val => 7.0), 
+         (Strify ("-"), new Value'(Kind => NumV, Val => 7.0),
          new Value'(Kind => NumV, Val => 2.0)).Val = 5.0);
       Assert (Eval
-         (Strify ("*"), new Value'(Kind => NumV, Val => 3.0), 
+         (Strify ("*"), new Value'(Kind => NumV, Val => 3.0),
          new Value'(Kind => NumV, Val => 2.0)).Val = 6.0);
       Assert (Eval
-         (Strify ("/"), new Value'(Kind => NumV, Val => 6.0), 
+         (Strify ("/"), new Value'(Kind => NumV, Val => 6.0),
          new Value'(Kind => NumV, Val => 2.0)).Val = 3.0);
 
-      
       --  Top Interp tests
-      Assert (Top_Interp (new ExprC'(Kind => IdC, Id => Strify ("true"))) = "true");
+      Assert (Top_Interp
+         (new ExprC'(Kind => IdC, Id => Strify ("true"))) = "true");
 
-      Put_Line (Top_Interp (new ExprC'(Kind => AppC, 
-      Fun => (new ExprC'(Kind => IdC, Id => (Strify ("+")))), 
-      Arg => (new ExprC'(Kind => NumC, Val => 3.0)),
-      Arg2 => (new ExprC'(Kind => NumC, Val => 3.0)))));
+      Assert (Top_Interp (new ExprC'(Kind => AppC,
+         Fun => (new ExprC'(Kind => IdC, Id => (Strify ("+")))),
+         Arg1 => (new ExprC'(Kind => NumC, Val => 3.0)),
+         Arg2 => (new ExprC'(Kind => NumC, Val => 3.0))))
+            = Float'Image (6.0));
 
-      Put_Line (Top_Interp (new ExprC'(Kind => AppC, 
-      Fun => (new ExprC'(Kind => IdC, Id => (Strify ("*")))), 
-      Arg => (new ExprC'(Kind => NumC, Val => 2.0)),
-      Arg2 => (new ExprC'(Kind => NumC, Val => 3.0)))));
+      Assert (Top_Interp (new ExprC'(Kind => AppC,
+         Fun => (new ExprC'(Kind => IdC, Id => (Strify ("*")))),
+         Arg1 => (new ExprC'(Kind => NumC, Val => 2.0)),
+         Arg2 => (new ExprC'(Kind => NumC, Val => 3.0))))
+            = Float'Image (6.0));
+
+      Assert (Top_Interp (new ExprC'(Kind => AppC,
+         Fun => LamC_Ex1,
+         Arg1 => (new ExprC'(Kind => NumC, Val => 0.0)),
+         Arg2 => (new ExprC'(Kind => NumC, Val => 5.0)))) = "true");
 
    end;
 end Zode5;
